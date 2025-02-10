68a826f6afa5e1cd6ac86d6952ba6429465891df
Atish019 <atish.sharma7321@gmail.com>
2025-02-11 00:27:54 +0530

    Initialize DVC and add sample dataset

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Just for Learning cookiecutter',
+    author='Atish019',
+    license='',
+)
